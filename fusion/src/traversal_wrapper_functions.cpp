399ab330ebe439a70f7e586618f941c588157c08
karrerm <karrerm@student.ethz.ch>
2016-01-25 18:27:04 +0100

    Added tree traversal function for point-cloud input data
    Just preleminary version--> untested!!!

--- fusion/src/traversal_wrapper_functions.cpp
+++ fusion/src/traversal_wrapper_functions.cpp
@@ -572,6 +572,140 @@ inline void transformLoopSimPrecalculatedNeg_vis
 	}
 }
 
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+// 25.01.2016: karrerm
+inline void transformLoopSimPrecalculatedNegPointCloud_subtree
+(
+		const float *qxp1, const float *qxp2, const float *qxp3,
+		const float *qyp1, const float *qyp2, const float *qyp3,
+		const float &t1, const float &t2, const float &t3,
+		const sidetype &_n, const sidetype &_bandwidth,
+		const sidetype &_brickLength,
+		const int &_imageWidth, const int &_imageHeight,
+		int3 &_boxMin, int3 &_boxMax,
+		const float *depthData,
+		const float *noiseData,
+		float scaling, float maxcamdistance,
+		volumetype *_tree,
+		volumetype &_nBranchesUsed,
+		const volumetype &_nLeavesTotal, volumetype &_nLeavesUsed, volumetype &_nLeavesQueued,
+		volumetype *_leafNumber, sidetype3 *_leafPos, sidetype *_leafScale,
+		volumetype *_queueIndexOfLeaf, uchar *_child, volumetype *_branchNumber
+		,ParentArray &_leafParent
+		,volumetype *anchorTreeBuds
+		,volumetype *anchorTreeBudsParentLeaf
+		,volumetype *anchorLeafBuds
+		,volumetype &_numberOfQueuedTreeBuds
+		,volumetype &_numberOfQueuedLeafBuds
+		,volumetype _treeSizeSinceMeshing
+)
+{
+	int vxt[_imageWidth];
+	int vyt[_imageWidth];
+	int vzt[_imageWidth];
+	for(int x=0;x<_imageWidth;x++) vxt[x] = vyt[x] = vzt[x] = -1;
+
+	for(int y=0;y<_imageHeight;y++){
+		int vxl = -1; int vyl = -1; int vzl = -1;
+		for(int x=0;x<_imageWidth;x+=4){
+			const int ptIndex = (y*_imageWidth+x);
+			const float pz = depthData(y*_imageWidth+x);
+			const float *noise = noiseData + (y*_imageWidth + x);
+			int vx[4] = {
+					(int)((qxp1[ptIndex  ] + qyp1[ptIndex  ]) + t1),
+					(int)((qxp1[ptIndex+1] + qyp1[ptIndex+1]) + t1),
+					(int)((qxp1[ptIndex+2] + qyp1[ptIndex+1]) + t1),
+					(int)((qxp1[ptIndex+3] + qyp1[ptIndex+1]) + t1)
+			};
+			int vy[4] = {
+					(int)((qxp2[ptIndex  ] + qyp2[ptIndex  ]) + t2),
+					(int)((qxp2[ptIndex+1] + qyp2[ptIndex+1]) + t2),
+					(int)((qxp2[ptIndex+2] + qyp2[ptIndex+2]) + t2),
+					(int)((qxp2[ptIndex+3] + qyp2[ptIndex+3]) + t2)
+			};
+			int vz[4] = {
+					(int)((qxp3[ptIndex  ] + qyp3[ptIndex  ]) + t3),
+					(int)((qxp3[ptIndex+1] + qyp3[ptIndex+1]) + t3),
+					(int)((qxp3[ptIndex+1] + qyp3[ptIndex+2]) + t3),
+					(int)((qxp3[ptIndex+1] + qyp3[ptIndex+3]) + t3)
+			};
+
+			for(unsigned int k=0;k<4;k++){
+				int xk = x+k;
+				if(((pz[k])>0.0f && (pz[k])<maxcamdistance)
+						&& ( (vx[k]&(-8)) != vxt[xk] || (vy[k]&(-8)) != vyt[xk] || (vz[k]&(-8)) != vzt[xk])
+						&& ( (vx[k]&(-8)) != vxl || (vy[k]&(-8)) != vyl || (vz[k]&(-8)) != vzl)
+				){
+					vxt[xk] = vxl = vx[k]&(-8); vyt[xk] = vyl = vy[k]&(-8); vzt[xk] = vzl = vz[k]&(-8);
+
+	//				sidetype3 pMin, pMax;
+					int temp = vx[k]-_bandwidth;
+					if(temp<0){
+	//				pMin.x = 0;
+						if(_boxMin.x > temp) _boxMin.x = temp;
+					}
+	//				else pMin.x = temp;
+					temp = vy[k]-_bandwidth;
+					if(temp<0){
+	//				pMin.y = 0;
+						if(_boxMin.y > temp) _boxMin.y = temp;
+					}
+	//				else pMin.y = temp;
+					temp = vz[k]-_bandwidth;
+					if(temp<0){
+	//				pMin.z = 0;
+						if(_boxMin.z > temp) _boxMin.z = temp;
+					}
+	//				else pMin.z = temp;
+					temp = vx[k]+_bandwidth;
+					if(temp>=_n){
+	//				pMax.x = _n-1;
+						if(_boxMax.x < temp) _boxMax.x = temp;
+					}
+	//				else pMax.x = temp;
+					temp = vy[k]+_bandwidth;
+					if(temp>=_n){
+	//							pMax.y = _n-1;
+						if(_boxMax.y < temp) _boxMax.y = temp;
+					}
+	//				else pMax.y = temp;
+					temp = vz[k]+_bandwidth;
+					if(temp>=_n){
+	//				pMax.z = _n-1;
+						if(_boxMax.z < temp) _boxMax.z = temp;
+					}
+	//				else pMax.z = temp;
+
+					if(vx[k]>=0 && (sidetype)vx[k]<_n && vy[k]>=0 && (sidetype)vy[k]<_n && vz[k]>=0 && (sidetype)vz[k]<_n){
+	#ifndef USE_NOISE_FOR_SCALE
+						//-- Use depth as indicator for the scale
+						queryPointDepthSingle_func_subtree(vx[k],vy[k],vz[k],leafScaleFunc(pz[k],_brickLength),
+								_n,_brickLength,_tree,_nBranchesUsed,_nLeavesTotal,_nLeavesUsed,_nLeavesQueued,
+								_leafNumber,_leafPos,_leafScale,_queueIndexOfLeaf,_leafParent,
+								anchorTreeBuds,anchorTreeBudsParentLeaf,anchorLeafBuds,
+								_numberOfQueuedTreeBuds,_numberOfQueuedLeafBuds,
+								_treeSizeSinceMeshing);
+	#else
+						//-- Use noise as indicator for the scale
+						queryPointDepthSingle_func_subtree(vx[k],vy[k],vz[k],leafScaleFuncNoise(noise[k],_brickLength),
+								_n,_brickLength,_tree,_nBranchesUsed,_nLeavesTotal,_nLeavesUsed,_nLeavesQueued,
+								_leafNumber,_leafPos,_leafScale,_queueIndexOfLeaf,_leafParent,
+								anchorTreeBuds,anchorTreeBudsParentLeaf,anchorLeafBuds,
+								_numberOfQueuedTreeBuds,_numberOfQueuedLeafBuds,
+								_treeSizeSinceMeshing);
+	#endif
+					}
+				} else {
+					vxt[xk] = vyt[xk] = vzt[xk] = vxl = vyl = vzl = -1;
+				}
+			}
+		}
+	}
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 inline void transformLoopSimPrecalculatedNeg_subtree
 (
 		const float *qxp1, const float *qxp2, const float *qxp3,
